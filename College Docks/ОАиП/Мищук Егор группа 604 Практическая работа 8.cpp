d99656d7b1fb358925b1a641ee68b3daaa1dae65
Egor <orlovegor17@gmail.com>
2022-01-13 18:53:58 +0700

    Files added and packed

---
+++ College Docks/ОАиП/Мищук Егор группа 604 Практическая работа 8.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	const int n = 7;
+	const int m = 7;
+	int A[n][m] = { {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7}, {1,2,3,4,5,6,7} };
+	int* arr = new int[n];
+	int temp0 = 0;
+	int temp1 = 0;
+	int temp2 = 0;
+	int temp3 = 0;
+	int temp4 = 0;
+	int temp5 = 0;
+	int temp6 = 0;
+	/*for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << "Enter" << "[" << i+1 << "]" << "[" << j+1 << "]" << "element of matrix: ";
+			cin >> A[i][j];
+		}
+	}*/
+	for (int i = 0; i < 7; i++)
+	{
+		if (A[0][i] > 0)
+		{
+			temp0 += A[0][i];
+		}
+		if (A[1][i] > 0)
+		{
+			temp1 += A[1][i];
+		}
+		if (A[2][i] > 0)
+		{
+			temp2 += A[2][i];
+		}
+		if (A[3][i] > 0)
+		{
+			temp3 += A[3][i];
+		}
+		if (A[4][i] > 0)
+		{
+			temp4 += A[4][i];
+		}
+		if (A[5][i] > 0)
+		{
+			temp5 += A[5][i];
+		}
+		if (A[6][i] > 0)
+		{
+			temp6 += A[6][i];
+		}
+	}
+	cout << temp0 << ' ' << temp1 << ' ' << temp2 << ' ' << temp3 << ' ' << temp4 << ' ' << temp5 << ' ' << temp6;
+	cout << endl;
+	arr[0] = temp0 / 7;
+	arr[1] = temp1 / 7;
+	arr[2] = temp2 / 7;
+	arr[3] = temp3 / 7;
+	arr[4] = temp4 / 7;
+	arr[5] = temp5 / 7;
+	arr[6] = temp6 / 7;
+	for (int i = 0; i < 7; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << setw(4) << A[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ College Docks/ОАиП/Оптимизированная лабораторная работа №8.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	const int n = 7;
+	const int m = 7;
+	/*double A[7][7] = { {555,-13,13,4,67,115,11 }, { 1,12,109,5,13,6,13 }, { 4,123,43,4,5,6,7 }, { 1,1,12,4,5,6,55 }, { 8,2,3,48,71,6,7 }, { 1,2,3,856,5,6,7 }, { 1,2,0,4,5,6,7 } };*/
+	double A[n][m];
+	double* B = new double[m];
+	double temp = 0;
+	for (int i = 0; i < n; i++) //заполнение матрицы
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << "Enter" << "[" << i + 1 << "]" << "[" << j + 1 << "]" << "element of matrix: ";
+			cin >> A[i][j];
+		}
+	}
+
+	for (int i = 0; i < n; i++) //вычисление вектора положительных чисел в каждом столбе
+	{
+		temp = 0;
+		for (int j = 0; j < m; j++)
+		{
+			if (A[j][i] > 0)
+			{
+				temp += A[j][i];
+			}
+			else {
+				continue;
+			}
+		}
+		B[i] = temp / m;
+	}
+	
+	double minimal = 0;
+	int MinIndex = 0;
+	double multi = 1;
+
+	for (int i = 0; i < m; i++) //нахождение минимального элемента и его индекса
+	{
+		if (B[MinIndex] > B[i])
+		{
+			MinIndex = i;
+		}
+	}
+	minimal = B[MinIndex];
+	cout << "Minimal element: " << minimal << endl; //вывод минимального элемента
+	for (int i = 0; i < m; i++) //вывод массива
+	{
+		cout << B[i] << " ";
+	}
+	cout << endl << endl; //нахождение произведения элементов вектора
+	for (int i = 0; i < MinIndex; i++)
+	{
+		multi *= B[i];
+	}
+	cout << "Multiply: " << multi << endl;
+	
+	for (int i = 0; i < n; i++) { // Вывод матрицы
+		for (int j = 0; j < m; j++) {
+			cout << setw(4) << A[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
